c8b275b9e5f582217bc69e50d7651a8a6375486e
bno4 <bnduchemin@gmail.com>
2023-05-23 11:21:28 +0200

    update

---
+++ src/pages/About.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Navigation from '../components/Navigation';
+import Logo from '../components/Logo';
+
+const About = () => {
+    return (
+        
+        <div>
+            <Logo/>
+            <Navigation/>
+            <h1>A propos</h1>
+            <br/>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum culpa sit rerum sequi praesentium. Odio dolorum ipsam, beatae vitae soluta at cum architecto praesentium quod consequuntur dolores illo distinctio. Voluptates!</p>
+<br/>
+<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Mollitia alias ipsum nostrum eligendi eos explicabo cupiditate, sed atque exercitationem ipsa totam aut cumque nam minima est accusantium ducimus fugiat at obcaecati dolorem, consectetur aperiam culpa quisquam blanditiis? Quos illo deserunt debitis, neque, explicabo vitae maiores, blanditiis esse quidem error tempora adipisci deleniti dolor corrupti ipsam natus laborum iure animi beatae cum ducimus praesentium temporibus eum. Repellendus nihil modi nemo ex expedita magni, nesciunt enim sapiente nam, excepturi perferendis aliquam tenetur. Vitae, ad? Ut, fugit molestiae magnam ad voluptates voluptatibus? Tempore nihil praesentium rem, ad debitis eos! Eaque, dolores illo veritatis ab iste recusandae ut laborum voluptatibus fuga dicta architecto consequuntur quasi accusamus officia molestiae, animi cumque? Ipsam reprehenderit modi quam dolorem? Reiciendis doloremque quaerat quam corrupti, ducimus harum sint temporibus quisquam explicabo suscipit eligendi? Fugiat labore omnis suscipit temporibus perferendis. Quod alias, cupiditate qui possimus id dolorem et ipsam magni unde perspiciatis eum soluta, dicta iure recusandae, odit inventore harum facilis! Enim soluta autem voluptates qui totam tenetur, aliquid iste beatae explicabo, nobis recusandae illo cupiditate cum perferendis maiores rerum deleniti ad ratione, dicta dolores vel odit mollitia! Dolor facere quasi pariatur, fugit voluptate reiciendis a harum ratione impedit nobis?</p>
+        </div>
+    );
+};
+
+export default About;
